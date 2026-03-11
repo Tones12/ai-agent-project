749f49d1fd1d10dab758c1f65d7f00d30d9d867b
Tony Travis <tony.travis12@gmail.com>
2026-03-11 18:03:50 +0000

    get_file_info function added

---
+++ functions/get_files_info.py
@@ -0,0 +1,31 @@
+import os
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    except Exception as e:
+        return f'Error getting absolute directories: {e}'
+    
+    try:
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        valid_directory = os.path.isdir(target_dir)
+    except Exception as e:
+        return f'Error validating directories: {e}'
+    
+    if valid_target_dir == False:
+        return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    if valid_directory == False:
+        return (f'Error: "{directory}" is not a directory')
+    
+    files = os.listdir(target_dir)
+
+    file_string_list = []
+    file_string = ''
+    for file in files:
+        file_size = os.path.getsize(f'{target_dir}/{file}')
+        is_dir = os.path.isdir(f'{target_dir}/{file}')
+        file_string_list.append(f'- {file}: file_size={file_size} bytes, is_dir={is_dir}\n')
+    
+    file_string = file_string.join(file_string_list)
+
+    return file_string
